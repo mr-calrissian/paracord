8d0279169956e2e435bf7e008a71268a7ee881e3
lando <calrissian@opayq.com>
2021-04-05 17:48:02 +0000

    add setImmediate back in gateway

--- src/clients/Gateway/Gateway.ts
+++ src/clients/Gateway/Gateway.ts
@@ -842,12 +842,12 @@ export default class Gateway {
           // back pressure may cause the interval to occur too late, hence this check
           this._checkIfShouldHeartbeat();
           this.handleEvent(type, data);
-          //   // defer execution to allow back pressure (which may include critical events like HEARTBEAT_ACK) to process
-          // setImmediate(() => {
-          //   // deferred events will block just as hard on the next pass of the event loop as when they were coming in, hence this second check
-          //   this._checkIfShouldHeartbeat();
-          //   this.handleEvent(type, data);
-          // });
+          // defer execution to allow back pressure (which may include critical events like HEARTBEAT_ACK) to process
+          setImmediate(() => {
+            // deferred events will block just as hard on the next pass of the event loop as when they were coming in, hence this second check
+            this._checkIfShouldHeartbeat();
+            this.handleEvent(type, data);
+          });
         } else {
           this.log('WARNING', `Unhandled packet. op: ${opCode} | data: ${data}`);
         }
